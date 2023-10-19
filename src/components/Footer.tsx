3e1f1013acf778a41ea94586984c0508fe110daa
forjmail <forj@mail.ru>
2023-10-19 20:20:15 +0400

    first bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,50 +17,50 @@ export default function Footer() {
     setCompleted(todos.filter((el) => el.completed === true).length);
   }, [todos, filter]);
 
-  return (
-    todos.length && (
-      <footer className="footer">
-        <span className="todo-count">
-          <strong>{remaining}</strong>
-          <span>{remaining === 1 ? ' item' : ' items'} left</span>
-        </span>
-        <ul className="filters">
-          <li>
-            <a
-              href="#/all"
-              className={`${filter === 'all' ? 'selected' : ''}`}
-              onClick={() => setFilter('all')}
-            >
-              All
-            </a>
-          </li>
-          <li>
-            <a
-              href="#/active"
-              className={`${filter === 'active' ? 'selected' : ''}`}
-              onClick={() => setFilter('active')}
-            >
-              Active
-            </a>
-          </li>
-          <li>
-            <a
-              href="#/completed"
-              className={`${filter === 'completed' ? 'selected' : ''}`}
-              onClick={() => setFilter('completed')}
-            >
-              Completed
-            </a>
-          </li>
-        </ul>
-        {completed ? (
-          <button className="clear-completed" onClick={removeCompleted}>
-            Clear completed
-          </button>
-        ) : (
-          <></>
-        )}
-      </footer>
-    )
+  return todos.length ? (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>{remaining}</strong>
+        <span>{remaining === 1 ? ' item' : ' items'} left</span>
+      </span>
+      <ul className="filters">
+        <li>
+          <a
+            href="#/all"
+            className={`${filter === 'all' ? 'selected' : ''}`}
+            onClick={() => setFilter('all')}
+          >
+            All
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/active"
+            className={`${filter === 'active' ? 'selected' : ''}`}
+            onClick={() => setFilter('active')}
+          >
+            Active
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/completed"
+            className={`${filter === 'completed' ? 'selected' : ''}`}
+            onClick={() => setFilter('completed')}
+          >
+            Completed
+          </a>
+        </li>
+      </ul>
+      {completed ? (
+        <button className="clear-completed" onClick={removeCompleted}>
+          Clear completed
+        </button>
+      ) : (
+        <></>
+      )}
+    </footer>
+  ) : (
+    <></>
   );
 }
